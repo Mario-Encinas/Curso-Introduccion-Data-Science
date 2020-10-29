32b1500470e1a507ff731ea2798590510c73ce99
Komsal <62627872+Mario-Encinas@users.noreply.github.com>
2020-10-28 21:25:26 -0600

    Update Ejercicio_2.sql

--- Sesion_2/Ejercicio_2.sql
+++ Sesion_2/Ejercicio_2.sql
@@ -10,5 +10,5 @@ SELECT COUNT(*) FROM articulo WHERE nombre LIKE '%Pasta%';
 #¿Cuál es el salario mínimo y máximo?
 SELECT MIN(salario) AS minimo, MAX(salario) AS maximo FROM puesto;
 
-¿Cuál es la suma del salario de los últimos cinco puestos agregados?
+#¿Cuál es la suma del salario de los últimos cinco puestos agregados?
 SELECT SUM(salario) AS total FROM (SELECT * FROM puesto order by id_puesto desc limit 5) AS puesto;
