385765e6ab7c7d49ead2d36bf434bf6accead098
Komsal <62627872+Mario-Encinas@users.noreply.github.com>
2020-10-28 21:24:12 -0600

    Add files via upload

--- Sesion_2/Ejercicio_1.sql
+++ Sesion_2/Ejercicio_1.sql
@@ -1,7 +1,16 @@
 USE tienda;
 
+#¿Qué artículos incluyen la palabra Pasta en su nombre?
 SELECT * FROM articulo WHERE nombre LIKE '%Pasta%';
+
+#¿Qué artículos incluyen la palabra Cannelloni en su nombre?
 SELECT * FROM articulo WHERE nombre LIKE '%Cannelloni%';
+
+#¿Qué nombres están separados por un guión (-) por ejemplo Puree - Kiwi?
 SELECT * FROM articulo WHERE nombre LIKE '%-%';
+
+#¿Qué puestos incluyen la palabra Designer?
 SELECT * FROM puesto WHERE nombre LIKE '%Designer%';
+
+#¿Qué puestos incluyen la palabra Developer?
 SELECT * FROM puesto WHERE nombre LIKE '%Developer%';
