ffdd94ae419bb0f47c4d42f46e3138082316d6e9
Komsal <62627872+Mario-Encinas@users.noreply.github.com>
2020-10-28 21:22:10 -0600

    Add files via upload

--- Sesion_2/Ejercicio_2.sql
+++ Sesion_2/Ejercicio_2.sql
@@ -1,6 +1,14 @@
 USE tienda;
 
+
+#¿Cuál es el promedio de salario de los puestos?
 SELECT AVG(salario) as Salario FROM puesto;
+
+#¿Cuántos artículos incluyen la palabra Pasta en su nombre?
 SELECT COUNT(*) FROM articulo WHERE nombre LIKE '%Pasta%';
+
+#¿Cuál es el salario mínimo y máximo?
 SELECT MIN(salario) AS minimo, MAX(salario) AS maximo FROM puesto;
+
+¿Cuál es la suma del salario de los últimos cinco puestos agregados?
 SELECT SUM(salario) AS total FROM (SELECT * FROM puesto order by id_puesto desc limit 5) AS puesto;
