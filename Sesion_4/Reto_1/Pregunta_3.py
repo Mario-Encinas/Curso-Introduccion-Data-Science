f38a630c5d43d3f8063ae9f59f0603fae786a3bc
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-09 22:08:06 -0600

    Update Pregunta_3.py

--- Sesion_4/Reto_1/Pregunta_3.py
+++ Sesion_4/Reto_1/Pregunta_3.py
@@ -1,5 +1,4 @@
-# Requires the PyMongo package.
-# https://api.mongodb.com/python/current
+#Nombre y contraseña de cada usuario.
 
 client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={}
