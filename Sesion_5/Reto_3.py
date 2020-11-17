17bcbe068529f377428ebf27ca6f72fcf916a31f
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-17 00:20:31 -0600

    Create Reto_3.py

---
+++ Sesion_5/Reto_3.py
@@ -0,0 +1,8 @@
+"""mediante el uso de agregaciones, encontrar el número de publicaciones que tienen
+conexión a Internet, sea desde Wifi o desde cable (Ethernet)."""
+
+client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+result = client['sample_airbnb']['listingsAndReviews'].aggregate([
+	{'$match': {'amenities': {'$in': ['Wifi', 'Ethernet']}}},
+	{'$group': {'_id': None, 'total': {'$sum': 1}}}
+])
