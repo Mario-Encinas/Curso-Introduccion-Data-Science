c5812c1bc2c981b1930870cbb0f305d216a10c34
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-22 16:07:19 -0600

    Update Pregunta_4.py

--- Sesion_5/Reto_1/Pregunta_4.py
+++ Sesion_5/Reto_1/Pregunta_4.py
@@ -1,6 +1,6 @@
 #Propiedades que no permitan fiestas ni fumadores
 
-client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+client = MongoClient('mongodb+srv://Encinas:****@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={
     'house_rules': re.compile(r"No smoking|No parties(?i)")
 }
