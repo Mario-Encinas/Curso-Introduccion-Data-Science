d35977213ad54cbc479b6e9f022ee4077fdf082c
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-22 16:05:43 -0600

    Update Reto_2.py

--- Sesion_5/Reto_2.py
+++ Sesion_5/Reto_2.py
@@ -1,7 +1,7 @@
 """agrega un filtro que permita obtener todas las publicaciones que tengan 50 o más comentarios,
 que la valoración sea mayor o igual a 80, que cuenten con conexión a Internet vía cable y estén ubicada en Brazil."""
 
-client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+client = MongoClient('mongodb+srv://Encinas:****@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={
     'number_of_reviews': {
         '$gte': 50
