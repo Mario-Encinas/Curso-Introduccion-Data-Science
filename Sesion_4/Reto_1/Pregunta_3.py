3eb64645d70bc11123cbe43d262f84bab32ba960
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-22 16:08:55 -0600

    Update Pregunta_3.py

--- Sesion_4/Reto_1/Pregunta_3.py
+++ Sesion_4/Reto_1/Pregunta_3.py
@@ -1,6 +1,6 @@
 #Nombre y contraseña de cada usuario.
 
-client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+client = MongoClient('mongodb+srv://Encinas:****@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={}
 project={
     'name': 1, 
