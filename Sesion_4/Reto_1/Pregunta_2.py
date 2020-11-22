c2c5059998c6f9add938a1a6b4dabb9864b56d3d
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-22 16:08:32 -0600

    Update Pregunta_2.py

--- Sesion_4/Reto_1/Pregunta_2.py
+++ Sesion_4/Reto_1/Pregunta_2.py
@@ -1,6 +1,6 @@
 #Título, elenco y año de cada película.
 
-client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+client = MongoClient('mongodb+srv://Encinas:****@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={}
 project={
     'title': 1, 
