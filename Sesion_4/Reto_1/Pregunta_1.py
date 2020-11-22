d218ba5bc06571fb78071a87e12a2e31235fc18d
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-22 16:08:11 -0600

    Update Pregunta_1.py

--- Sesion_4/Reto_1/Pregunta_1.py
+++ Sesion_4/Reto_1/Pregunta_1.py
@@ -1,6 +1,6 @@
 #Fecha, nombre y texto de cada comentario.
 
-client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+client = MongoClient('mongodb+srv://Encinas:****@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={}
 project={
     'date': 1, 
