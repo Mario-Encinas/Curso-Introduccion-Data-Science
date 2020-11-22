8e01bf7e49837d10b11e3c313db7b4852bc81a35
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-22 16:06:36 -0600

    Update Pregunta_2.py

--- Sesion_5/Reto_1/Pregunta_2.py
+++ Sesion_5/Reto_1/Pregunta_2.py
@@ -1,7 +1,7 @@
 #Propiedades que admitan mascotas.
 
 
-client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+client = MongoClient('mongodb+srv://Encinas:****@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={
     'house_rules': re.compile(r", Pets Allowed(?i)")
 }
