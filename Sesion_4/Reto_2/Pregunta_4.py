985bc686c7a304a2417c1b79289ce45d3e7dddb3
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-22 16:10:22 -0600

    Update Pregunta_4.py

--- Sesion_4/Reto_2/Pregunta_4.py
+++ Sesion_4/Reto_2/Pregunta_4.py
@@ -1,6 +1,6 @@
 #¿Cuál es título de las cinco películas más comentadas?
 
-client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+client = MongoClient('mongodb+srv://Encinas:****@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={}
 project={
     'title': 1, 
