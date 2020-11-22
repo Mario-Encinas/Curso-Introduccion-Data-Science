3e52b63dd545370846f50268402fea28e3630e5e
Mario Encinas <62627872+Mario-Encinas@users.noreply.github.com>
2020-11-22 16:10:01 -0600

    Update Pregunta_3.py

--- Sesion_4/Reto_2/Pregunta_3.py
+++ Sesion_4/Reto_2/Pregunta_3.py
@@ -1,6 +1,6 @@
 #¿Cuál es el máximo número de comentarios en una película?
 
-client = MongoClient('mongodb+srv://Encinas:Plch353p1@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+client = MongoClient('mongodb+srv://Encinas:****@cursobedu.ftknr.mongodb.net/test?authSource=admin&replicaSet=atlas-1gv35t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
 filter={}
 project={
     'num_mflix_comments': 1
